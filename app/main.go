c71ce2e4b1dc51087f3476677851db2398986bfe
thai-vinh-tran <harrisvincentjin@gmail.com>
2025-08-29 16:00:35 +0700

    ravid update tag

--- app/main.go
+++ app/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+    "encoding/json"
     "net/http"
+    "os"
 )
 
 func main() {
     http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
         w.WriteHeader(http.StatusOK)
         w.Write([]byte("OK"))
+    })
+
+    http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+        version := os.Getenv("GIT_TAG")
+        if version == "" {
+            version = "unknown"
+        }
+        resp := map[string]string{"version": version}
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(resp)
     })
 
     http.ListenAndServe(":6699", nil)
